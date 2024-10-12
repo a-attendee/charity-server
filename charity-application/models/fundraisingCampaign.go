0248eb354d660a1562b5badffd011256df9a5e7c
klawdee <klawdee@mail.ru>
2024-10-12 18:42:03 +0300

    added diagrams

--- charity-application/models/fundraisingCampaign.go
+++ charity-application/models/fundraisingCampaign.go
@@ -2,26 +2,25 @@ package models
 
 import (
 	"time"
-
 )
 
 type FundraisingCampaignStatus int8
 
 const (
-    Planned FundraisingCampaignStatus = iota
-    Holding
-    Completed
+	Planned FundraisingCampaignStatus = iota
+	Holding
+	Completed
 )
 
-type FundraisingCampaign struct { 
-    ID uint `gorm: "primaryKey"`
+type FundraisingCampaign struct {
+	ID uint `gorm: "primaryKey"`
 
-    Name string
-    EventTimeStart time.Time
-    Description string
-    ExpectedFundraising int
-    ActualFundraising int
-    Status FundraisingCampaignStatus
+	Name           string
+	EventTimeStart time.Time
+	Description    string
+	ExpectedFundraising int
+	ActualFundraising   int
+	Status              FundraisingCampaignStatus
 
-    Users []User `gorm: "many2many:user_fundraising_campaign; foreignKey:id: References:ID"`
+	Users []User `gorm: "many2many:user_fundraising_campaign; foreignKey:id: References:ID"`
 }
